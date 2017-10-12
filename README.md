2015c1bad6e1c14fda1d5a8c8bd628539c2e4bd6
Pavel Podkorytov <pod.pavel@gmail.com>
2017-10-12 16:40:27 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+roundcube-export-contacts
+=========================
+
+Export roundcube contacts in vcard format from mysql database.
+
+Saves all exported data from "contacts" table, "vcard" column to files like
+'./out/{EMAIL}_{USERID}.vcf'
---
+++ roundcube-export-contacts.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Export roundcube contacts in vcard format from mysql database.
+"""
+
+import argparse
+import codecs
+import json
+import mysql.connector
+import os
+import os.path
+from collections import namedtuple
+
+
+DEFAULT_CONFIG = "./config.json"
+DEFAULT_OUT = "./out"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="json config", default=DEFAULT_CONFIG)
+    parser.add_argument("--out", help="output directory path", default=DEFAULT_OUT)
+    return parser.parse_args()
+
+
+def load_config(path):
+    with open(path, "r") as fobj:
+        config = json.load(fobj)
+    return config
+
+
+def get_users(mysql_cnx):
+    """Get all 'user_id' and 'username' from 'users' table.
+
+    Args:
+        mysql_cnx: connection to mysql database
+
+    Returns:
+        mysql_users: sql query result
+    """
+
+    mysql_cur = mysql_cnx.cursor()
+    query = ("SELECT user_id, username FROM users")
+
+    mysql_cur.execute(query)
+    mysql_users = mysql_cur.fetchall()
+
+    return mysql_users
+
+
+def get_contacts(mysql_cnx, user):
+    """Get 'email', 'vcard', 'words' and 'del' from contacts table.
+
+    Args:
+        mysql_cnx: connection to mysql database
+        user: User namedtuple
+
+    Returns:
+        mysql_contacts: sql query result
+    """
+
+    mysql_cur = mysql_cnx.cursor()
+
+    query = ("SELECT email, vcard, words, del FROM contacts WHERE user_id = %s")
+    data = (user.id, )
+
+    mysql_cur.execute(query, data)
+    mysql_contacts = mysql_cur.fetchall()
+
+    return mysql_contacts
+
+
+def save_vcard(dirname, user, contact):
+    """Save data from vcard field to file.
+
+    Args:
+        dirname: output directory
+        user: User namedtuple
+        contact: Contact namedtuple
+    """
+
+    filename = "%s_%s.vcf" % (user.email, user.id)
+    out = os.path.join(dirname, filename)
+
+    if os.path.exists(out):
+        print "Append to file %s" % out
+    else:
+        print "Create file: %s" % out
+
+    with codecs.open(out, "a", "utf-8") as fobj:
+        fobj.write(contact.vcard)
+        fobj.write("\n")
+
+
+def main():
+    args = parse_args()
+    config = load_config(args.config)
+
+    if not os.path.exists(args.out):
+        os.mkdir(args.out)
+
+    # connect to databases
+    mysql_cnx = mysql.connector.connect(buffered=True, **config["mysql"])
+
+    # create basic structures
+    User = namedtuple("User", ["id", "email"])
+    Contact = namedtuple("Contact", ["email", "vcard", "words", "deleted"])
+
+    users = get_users(mysql_cnx)
+    for user in users:
+        user = User(user[0], user[1])
+        contacts = get_contacts(mysql_cnx, user)
+        for contact in contacts:
+            contact = Contact(contact[0], contact[1], contact[2], contact[3])
+            save_vcard(args.out, user, contact)
+
+    mysql_cnx.close()
+
+
+if __name__ == "__main__":
+    main()
